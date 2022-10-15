08a199080fda1b6dfeec03df9d530aef33f5ee16
adebayo wisdom <adebayowisdom796@gmail.com>
2022-10-14 22:24:44 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Block
+ * Return: Always 0
+ */
+main(void)
+{
+	char lc;
+
+	for (lc = 'a'; lc <= 'z'; lc++)
+
+		putchar(lc);
+
+	for (lc = 'A'; lc <= 'Z' lc++)
+
+		putchar(lc);
+	printchar('\n');
+
+	return (0);
+
+}
