5b3262ce755b823ee75e26b3a427043ae9a58ce3
adebayo wisdom <adebayowisdom796@gmail.com>
2022-10-15 18:59:15 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - print numbers 
+ * main - print numbers 0 - 9 
  * Return: Always 0
  */
 int main(void)
@@ -8,9 +8,8 @@ int main(void)
 	char nb;
 
 	for (nb = 0; nb < 10; nb++)
-	{
 		putchar(nb);
-	}
+
 	putchar('\n');
 	return (0);
 }
