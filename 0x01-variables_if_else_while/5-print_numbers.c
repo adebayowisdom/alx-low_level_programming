3710088adb2dcedd0b1c987e12237d208f848a9d
adebayo wisdom <adebayowisdom796@gmail.com>
2022-10-15 18:43:26 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print numbers 
+ * Return: Always 0
+ */
+int main(void)
+{
+	char nb;
+
+	for (nb = 0; nb < 10; nb++)
+	{
+		putchar(nb);
+	}
+	putchar('\n');
+	return (0);
+}
