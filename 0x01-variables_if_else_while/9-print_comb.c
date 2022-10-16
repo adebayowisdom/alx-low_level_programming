9182ce735784a1546904cc8036e06eaa4df9be6e
adebayo wisdom <adebayowisdom796@gmail.com>
2022-10-16 09:12:56 -0700

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Block
+ * Return: Always 0
+ */
+int main(void)
+{
+	int lc;
+
+	for (lc = 48; lc < 58; lc++)
+	{
+		putchar(lc);
+		if (lc != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		putchar('\n');
+		return (0);
+	}
