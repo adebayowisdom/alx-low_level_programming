00b64d30b32262176269412b016830593f5a3160
adebayo wisdom <adebayowisdom796@gmail.com>
2022-10-15 20:01:47 -0700

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print lower case in reverse
+ * Return: Always 0
+ */
+int main(void)
+{
+	char lc;
+
+	for (lc = 'z'; lc < 'a'; lc++)
+		putchar(lc);
+	putchar('\n');
+	return (0);
+}
