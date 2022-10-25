7656c271b85c646854884500cf130407e0154e54
adebayo wisdom <adebayowisdom796@gmail.com>
2022-10-24 18:05:33 -0700

    1 task

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_two_int - swaps the values of two integer
+ * @b: first pointer
+ * @a: second pointer
+ */
+
+void swap_int(int *a, int *b)
+{
+	int constant = *a;
+	*a = *b;
+	*a = constant;
+}
+
