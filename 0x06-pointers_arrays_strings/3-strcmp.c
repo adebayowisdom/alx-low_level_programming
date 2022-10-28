6b037576a71d3025b964ec4b345ecd239da5ff3c
adebayo wisdom <adebayowisdom796@gmail.com>
2022-10-27 17:14:43 -0700

    correction

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * _strcmp - compare two strings values
@@ -15,7 +16,7 @@ int _strcmp(char *s1, char *s2)
 	{
 		if (s1[i] != s2[i])
 		{
-			return(s1[i] -s2[i]);
+			return(s1[i] - s2[i]);
 		}
 		i++;
 	}
